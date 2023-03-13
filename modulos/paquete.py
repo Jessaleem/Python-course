8a0643a62edf9465d0d966c4b12e004f6e50d5f5
Jessica Martinez Alee <aleejess@gmail.com>
2023-03-12 22:52:18 -0500

    paquetes pyhton

---
+++ modulos/paquete.py
@@ -0,0 +1,5 @@
+import paquete.saludar
+print(paquete.saludar.saludar("Jess"))
+
+
+#print(paquete.__path__) Para acceder a la dirección del paquete
---
+++ modulos/paquete/saludar_2.py
@@ -0,0 +1,2 @@
+def saludar2(name):
+  return f"Hola {name}! Todo está bien y va a ponerse aún mejor, confía!"
