e98ef72ce3708ab45cfc820d6f41d645ee1c0f64
Jessica Martinez Alee <aleejess@gmail.com>
2023-03-22 11:18:53 -0500

    problemas con csv

--- problemas_csv/problema_csv.py
+++ problemas_csv/problema_csv.py
@@ -7,3 +7,19 @@ df["edad"] = df["edad"].astype(str)
 
 #Mostrar el tipo de datos del primer elemento de la columna edad
 print(type(df["edad"][0]))
+
+
+#Reemplazar el nombre "Jessica" por "Paola" con replace
+df["nombre"].replace("Jessica","Paola",inplace=True)
+#print(df["nombre"])
+
+#Elimina las filas con datos faltantes, para eliminar las columnas se pasa como parámetro axis=1
+df = df.dropna()
+#print(df)
+
+#Elimina filas repetidas
+df = df.drop_duplicates()
+#print(df)
+
+#Crear un csv con el df resultante (limpio)
+df.to_csv("problemas_csv\\datos_limpios.csv") 
