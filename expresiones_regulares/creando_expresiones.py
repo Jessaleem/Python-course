1b5a1807d76ec8c79cb0786ebc7c0788dbdd547a
Jessica Martinez Alee <aleejess@gmail.com>
2023-03-28 11:47:51 -0500

    expresiones regulares py

--- expresiones_regulares/creando_expresiones.py
+++ expresiones_regulares/creando_expresiones.py
@@ -1,3 +1,11 @@
 #Decectar un numero CABA y ocultarlo
 
-texto = "Hola Pedro, mi numero es:+54 114321-4321"
+import re
+
+texto = "Hola Pedro, mi numero es:+54 11 4321-4321"
+
+pattern = r"\+\d{2}\s\d{2}\s\d{4}-\d{4}"
+
+reemplazo = re.sub(pattern, "(Número oculto)",texto)
+
+print(reemplazo)
